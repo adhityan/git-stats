b7a85bdaf4b86b9c15ada5ed8ac88aaa1d0d0614
adhityan <adhityan@hotmail.com>
2019-08-12 10:44:13 +0400

    feat: base setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-import { Service } from 'typedi';
+import * as bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
-@Service()
-export class GitStats {}
+import Helmet from 'helmet';
+import Express from 'express';
+
+import { Config } from './config';
+
+const app: Express.Application = Express();
+
+const start = async () => {
+    app.use(Helmet());
+    app.use(cookieParser(Config.secret));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+    app.use(bodyParser.json({ limit: '50mb' }));
+
+    app.listen(Config.port, () => {
+        console.log(`server started on port ${Config.port}`);
+    });
+};
+
+start();
